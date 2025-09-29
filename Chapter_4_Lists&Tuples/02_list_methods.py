1fa45d261fde3d7d96e2f4cc0b7f0cc21b7eb195
Siddharth Vaishnav <demonreynard@gmail.com>
2025-09-29 15:41:10 +0530

    Add files via upload

---
+++ Chapter_4_Lists&Tuples/02_list_methods.py
@@ -0,0 +1,14 @@
+friends = ["Apple", "Orange", 5, 345.06, False, "Aakash", "Rohan"]
+
+print(friends)
+friends.append("Sidd")
+print(friends)
+
+L1 = [1,23, 45, 2, 4]
+# L1.sort()
+# L1.reverse()
+# L1.insert(3, 33333)
+# print(L1.pop(3))
+# L1.pop(3)
+L1.remove(45)
+print(L1)
---
+++ Chapter_4_Lists&Tuples/04_tuple_methods.py
@@ -0,0 +1,9 @@
+a = (1,23,54,3434,False, "Rohan", "Shivam")
+print(a)
+ 
+no = a.count(54)
+print(no)
+
+i = a.index(54)
+print(i)
+
