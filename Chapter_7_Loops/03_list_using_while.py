20d2ae85d0b89df9a14d6c7a53638baa5086cf40
Siddharth Vaishnav <demonreynard@gmail.com>
2025-10-02 17:37:34 +0530

    Add files via upload

---
+++ Chapter_7_Loops/03_list_using_while.py
@@ -0,0 +1,7 @@
+l = [1, "Sidd", False, "This", "Rohon"]
+
+i = 0
+while(i<len(l)):
+    print(l[i])
+    i +=1 # or i = i + 1
+    
---
+++ Chapter_7_Loops/06_for_with_else.py
@@ -0,0 +1,6 @@
+l = [1,7,4]
+
+for item in l:
+    print(item)
+else:
+    print("done") # this is printed with loop exhausts!
