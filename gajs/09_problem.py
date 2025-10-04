062a9fa6a8c2341860220cb6e99097030f109316
Siddharth Vaishnav <demonreynard@gmail.com>
2025-10-04 16:18:32 +0530

    Delete gajs directory

--- gajs/09_problem.py
+++
@@ -1,7 +0,0 @@
-def pattern(n):
-    if(n==0):
-        return
-    print("*" * n)
-    print(n-1)
-
-print(pattern(3))
--- gajs/11_problem.py
+++
@@ -1,9 +0,0 @@
-def rem(l, word):
-    n = []
-    for item in l:
-        if not(item == word):
-            n.append(item.strip(word))
-    return n
-
-l = ["Sidd", "Rohon", "Shubham", "an"]
-print(rem(l, "an"))
