239a396cfc58ecee6027c65422ee7917b3721a5b
Siddharth Vaishnav <demonreynard@gmail.com>
2025-10-04 16:20:32 +0530

    Add files via upload

---
+++ sfknfg/07_break_and_continue.py
@@ -0,0 +1,9 @@
+for i in range(100):
+    if(i == 37):
+        break # Exit the loop right now
+    print(i)
+    
+for i in range(100):
+    if(i ==37):
+        continue # Skip this iteration
+    print(i)
---
+++ sfknfg/10_problem.py
@@ -0,0 +1,5 @@
+l = ["Harry", "Sidd", "Rahul", "Sachin"]
+
+for name in l:
+    if(name.startswith("S")):
+        print(f"Hello {name}")
