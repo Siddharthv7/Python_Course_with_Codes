784c6f11040331b8ee22c52977ddc57189b735f8
Siddharth Vaishnav <demonreynard@gmail.com>
2025-10-06 10:28:48 +0530

    Delete OPPs directory

--- OPPs/02_instance_vs_class_attribute.py
+++
@@ -1,8 +0,0 @@
-class Employee:
-    language = "python" # This is class attribute
-    salary = 1700000
-    
-Sidd = Employee()
-Sidd.language = "Javascript" # This is instence attribute
-print(Sidd.language, Sidd.salary)
-
